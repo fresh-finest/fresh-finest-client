901f660ab503eef067b6f1160f7792d54c04a251
fresh-finest <bb@brecx.com>
2024-07-08 18:47:27 +0600

    Auth integrated with Login UI

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -3,26 +3,52 @@ import axios from 'axios';
 import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
 import './Login.css';
 import logo from '../../../assets/images/logo.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { signInFailure, signInStart, signInSuccess } from '../../../redux/user/userSlice';
 
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const [formData,setFormData] = useState({});
 
-    
+  const {loading,error} = useSelector((state)=>state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://localhost:5000/api/auth/signin', { email, password });
-            console.log('Login successful:', response.data);
-            // Handle successful login, e.g., save token, redirect
-        } catch (error) {
-            setError('Login failed. Please check your credentials and try again.');
-            console.error('Error logging in:', error);
-        }
-    };
+  const handleChange = (e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    try {
+      dispatch(signInStart);
+
+      const res = await fetch("http://localhost:5000/api/auth/signin",{
+        method:"POST",
+        headers:{
+          "Content-Type": "application/json",
+        },
+        body:JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if(data.success === false){
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate("/dashboard");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  }
+
+
 
     return (
         <Container className="d-flex justify-content-center align-items-center vh-100">
@@ -33,22 +59,22 @@ const Login = () => {
                   <Card.Title className="text-center">
                     <img src={logo} alt="Fresh Finest" className="mb-4 logo" />
                   </Card.Title>
-                  <Form>
+                  <Form onSubmit={handleSubmit}>
                     <Form.Group controlId="formBasicEmail">
                       <Form.Label>Email</Form.Label>
-                      <Form.Control type="email" placeholder="yourmail@mail.com" />
+                      <Form.Control type="email" placeholder="Enter your email" id='email' onChange={handleChange} />
                     </Form.Group>
     
                     <Form.Group controlId="formBasicPassword" className="mt-3">
                       <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" placeholder="Password" />
+                      <Form.Control type="password" placeholder="Password" id='password' onChange={handleChange} />
                     </Form.Group>
     
                     <Form.Group controlId="formBasicCheckbox" className="mt-3">
                       <Form.Check type="checkbox" label="Remember me" />
                     </Form.Group>
     
-                    <Button variant="success" type="submit" className="w-100 mt-3">
+                    <Button  variant="success" type="submit" className="w-100 mt-3">
                       Login
                     </Button>
     
